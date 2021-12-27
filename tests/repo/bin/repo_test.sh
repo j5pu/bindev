28595b0ad47b3f7c15dc5efc4dd563ca0bdce43d
j5pu <63794670+j5pu@users.noreply.github.com>
2021-12-27 16:06:53 +0100

    0.3.1: build

--- tests/repo/bin/repo_test.sh
+++ tests/repo/bin/repo_test.sh
@@ -1,8 +1,23 @@
 # shellcheck shell=sh disable=SC3045
 
 #
-# repo_test.sh is a test library with one function repo_test_function a
+# repo_test.sh is a test library with one function repo_test_function
 
-repo_test_function() {
+#######################################
+#  this is the description of function a with spaces and .
+#######################################
+repo_test_function_a() {
   :
 }
+
+#######################################
+#  this is the description of function b with spaces and .
+#######################################
+repo_test_function_b() (
+  :
+)
+
+#######################################
+#  this is the description of function c with spaces and .
+#######################################
+repo_test_function_c() if true; then true; fi
